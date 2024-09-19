14524db431d25b45f7c43e53b4795c59e390f88d
0786armank <0786armank@example.com>
2024-09-19 15:34:04 +0530

    first bst file commited

---
+++ bst.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+    
+};
+int main(){
+    //create bst
+    return 0;
+}
